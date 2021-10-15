b7acc295606d7b0c503faebfa0c34e325eb70d16
Nifilia <Anderokore@gmail.com>
2021-10-15 11:15:41 +0300

    oojs 2

--- oojs.js
+++ oojs.js
@@ -1,8 +1,22 @@
-function Shape(name, sides, sideLength){
-	this.name = name;
-	this.sides = sides;
-	this.sideLength = sideLength;
+function shape(name, sides, sideLength){
+	const obj = {};
+	obj.name = name;
+	obj.sides = sides;
+	obj.sideLength = sideLength;
+	return obj;
 }
+const square = new shape("square");
+square.name;
+square.sides;
+square.sideLength;
+console.log(shape("square", 4, 5))
+
+const triangle = shape("triangle");
+triangle.name;
+triangle.sides;
+triangle.sideLength;
+console.log(shape("square", 3, 3))
+/*
 Shape.prototype.calcPerimeter = function(){
 	console.log(this.sides * this.sideLength)
 }
@@ -10,3 +24,4 @@ var square = new Shape("square", 4, 5);
 square.calcPerimeter();
 var triangle = new Shape("triangle", 3, 3);
 triangle.calcPerimeter
+*/
